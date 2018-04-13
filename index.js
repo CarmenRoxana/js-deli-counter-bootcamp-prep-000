f95fa7fbf30b09e4c40f7fd950221e09a79e337b
Roxana <roxanafetea@yahoo.com>
2018-04-13 04:44:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function nowServing(katzDeliLine) {
   }
 }  
 
+function currentLine(katzDeli) {
+  for (let i = 0; i < katzDeli.length; i++) {
+   line.push(` `+[i+1]+`. `  + katzDeli[i])
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+ return(`The line is currently:` + line);
+}
