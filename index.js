8b0539d2de6f3ff76d20e8dbef63dc85e9d815a8
Roxana <roxanafetea@yahoo.com>
2018-04-13 04:34:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,41 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, newPerson){
+  
+  katzDeliLine.push(newPerson);
+  var index = katzDeliLine.length;
+   return "Welcome, "+newPerson+". You are number " +index+ " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  var name=katzDeliLine.shift();
+  if(katzDeliLine.length>0) {
+    return  'Currently serving ' +name+'.';
+  }else{
+    return "There is nobody waiting to be served!";
+  }
+}  
+
+function currentLine(katzDeliLine) {
+ var newLine=[];
+ var str='The line is currently: ';
+  if(katzDeliLine.length>0) {
+    for(let i=0;i<katzDeliLine.length;i++){
+   newLine[i] = `${[i+1]}. ${katzDeliLine[i]}`; 
+  } 
+  else {
+    return "The line is currently empty.";
+  }
+}
+}
+function currentLine(katzDeliLine){
+ var str = `The line is currently: `;
+ var newLine = [];
+  if (katzDeliLine.length === 0) return "The line is currently empty.";
+ else{
+   for (let i = 0; i < katzDeliLine.length; i++){
+       newLine[i] = `${[i+1]}. ${katzDeliLine[i]}`;      }
+    str += newLine.join(', ')
+    return str;
+  }
+}
